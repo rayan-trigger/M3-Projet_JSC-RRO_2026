feecefbc8b487c8ebddee6cbd8b40261528d0c47
Julien-Schneiter <julien.schneiter@divtec.ch>
2026-05-05 11:37:03 +0200

    test

--- M3_RRO_JSC/obj/Release/net10.0-windows/M3_RRO_JSC.AssemblyInfo.cs
+++ M3_RRO_JSC/obj/Release/net10.0-windows/M3_RRO_JSC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("M3_RRO_JSC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49c4c85105c9430e573a41d4df862ac472663190")]
 [assembly: System.Reflection.AssemblyProductAttribute("M3_RRO_JSC")]
