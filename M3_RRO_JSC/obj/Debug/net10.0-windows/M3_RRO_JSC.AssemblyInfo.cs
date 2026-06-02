991368aed3a1c8b2fd587ff8cd46ac832f4fa4dc
Julien-Schneiter <julien.schneiter@divtec.ch>
2026-06-02 16:37:36 +0200

    hz

--- M3_RRO_JSC/obj/Debug/net10.0-windows/M3_RRO_JSC.AssemblyInfo.cs
+++ M3_RRO_JSC/obj/Debug/net10.0-windows/M3_RRO_JSC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("M3_RRO_JSC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+127b36bb1eb597c30f70a23cfe698c2033a4ac3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85da782f07b9fd16f72033b989c2017baf429922")]
 [assembly: System.Reflection.AssemblyProductAttribute("M3_RRO_JSC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("M3_RRO_JSC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
